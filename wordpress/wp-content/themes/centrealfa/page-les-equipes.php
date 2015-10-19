3581ec9d794e8b140417ff4da86339fb09503eed
Anne-Sophie Pirlet <annesophie.pirlet@gmail.com>
2015-10-19 16:29:09 +0200

    formulaire ninja form inscription tabalfa

--- wordpress/wp-content/themes/centrealfa/page-les-equipes.php
+++ wordpress/wp-content/themes/centrealfa/page-les-equipes.php
@@ -12,22 +12,27 @@
          ?>
        <?php the_content(); ?>
      <?php endwhile;endif;?>
-
-        <?php $args=array('order'=>'ASC','hide_empty'=>0);$equipes=(get_terms('equipes',$args));
-        var_dump($equipes);
-        foreach($equipes as $equipe):
-        ?>
+        <?php $args=array('taxonomy'=>'equipes','hide_empty'=>'0','hierarchical'=>1,'orderby'=>'id');$equipes=get_categories($args); ?>
+        <?php
+        foreach($equipes as $equipe):?>
         <h2 class="subtitle"><?php echo $equipe->name;?></h2>
          <div class="equipe layout">
-           <?php $args = array( 'post_type' => 'employes','tax_query'=>array(array('taxonomy'=>'equipes','field'=>'slug','terms'=>$equipe->slug) ));
-            $the_query = new WP_Query( $args );
-            if ( $the_query->have_posts() ) :
-                while ( $the_query->have_posts() ) : $the_query->the_post();?>
+           <?php
+           $args = array( 'post_type' => 'employes' );
+           $the_query = new WP_Query( $args );
+            if ( $the_query->have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post();?>
                   <?php $tax_names=array();$taxs_id=get_field('fonction');
                       foreach($taxs_id as $tax_id){
                         array_push($tax_names,get_term($tax_id,'fonction'));
                       }
                     ?>
+                    <?php $equipes_names=array();$equipe_ids=get_field('equipes');
+                        foreach($equipe_ids as $equipe_id){
+                          array_push($equipes_names,get_term($equipe_id,'equipes')->name);
+                        }
+                        if(in_array($equipe->name,$equipes_names)):
+                      ?>
+
                     <div class="layout__item u-3/10 equipe__item"><img src="<?php echo get_field('photo')?>" alt="" class="equipe__img">
                       <h3 class="equipe__nom"><?php the_title();?></h3>
                       <div class="equipe__poste">
@@ -36,7 +41,7 @@
                       <?php endforeach;?>
                       </div>
                     </div>
-           <?php endwhile;endif;?>
+           <?php endif;endwhile;endif;?>
          </div>
          <?php endforeach;?>
       </div>
